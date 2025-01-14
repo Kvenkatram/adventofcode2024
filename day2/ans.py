c60e6970c26ba2d259c8a3d439162dffbc7333ac
karthik <karthik.venkatram1@gmail.com>
2025-01-14 12:18:02 -0700

    Adding solution for part2

--- day2/ans.py
+++ day2/ans.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python3
 
-def is_safe(report:list) -> bool:
+def is_safe(report:list):
     
-    is_increasing = True
-    if report[0] > report[1]:
-        is_increasing = False
-
-    for index, item in enumerate(report[:-1]):
-        if is_increasing:
-            if item > report[index+1]:
-                return False
-        else:
-            if item < report[index+1]:
-                return False
-                
+    if sorted(report) != report and sorted(report) != report[::-1]:
+        return False
+    
+    for index, item in enumerate(report[:-1]):       
         diff = abs(item - report[index+1])
         if diff == 0 or diff > 3:
             return False
@@ -21,23 +13,36 @@ def is_safe(report:list) -> bool:
     return True
 
 
+def problem_dampen(report) -> bool:
+
+    for index, item in enumerate(report):
+        copy_report = report.copy()
+        copy_report.pop(index)
+        if is_safe(copy_report):
+            return True
     
-    
+    return False
 
 
 if __name__ == "__main__":
     
     # Part 1 and part 2
     safe = 0
-    safe_with_problem_damp = 0
+    pd = 0
     with open("input.txt") as fd:
         for line in fd:
             report_str = line.split()
             report = [int(item) for item in report_str]
-            if is_safe(report):
+            safe_report = is_safe(report)
+            if safe_report:
                 safe += 1
+            elif problem_dampen(report):
+                pd += 1
+                
+            
     
     print("Number of safe reports: {}".format(safe))
+    print("Number of safe reports with problem damping: {}".format(safe + pd))
 
             
 
